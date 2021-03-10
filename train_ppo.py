6e960c470f06a7cd14ffa9bbf03a70d0d4cfde9c
confiwent <kanif@outlook.com>
2021-03-11 00:17:52 +0800

    fix ppo

--- train_ppo.py
+++ train_ppo.py
@@ -8,17 +8,18 @@ import torch
 import torch.optim as optim
 from torch.autograd import Variable
 import logging
-from model_torch import ActorCritic
-from test_torch import valid, test
+from model_ac_torch import Actor, Critic
+from test_ppo_torch import valid, test
 import env
 import load_trace
 from replay_memory import ReplayMemory
 
-RANDOM_SEED = 52
+RANDOM_SEED = 28
 S_INFO = 6
 S_LEN = 8
 A_DIM = 6
-LEARNING_RATE = 0.0001
+LEARNING_RATE_ACTOR = 0.0001
+LEARNING_RATE_CRITIC = 0.001
 # TRAIN_SEQ_LEN = 100  # take as a train batch
 VIDEO_BIT_RATE = [300,750,1200,1850,2850,4300]  # Kbps
 BUFFER_NORM_FACTOR = 10.0
@@ -30,7 +31,7 @@ DEFAULT_QUALITY = 1  # default video quality without agent
 # RANDOM_SEED = 42
 # GAMMA = 0.90
 # ENTROPY_WEIGHT = 0.99
-UPDATE_INTERVAL = 1000
+UPDATE_INTERVAL = 100
 RAND_RANGE = 1000
 ENTROPY_EPS = 1e-6
 MIN_MOMERY = 100
@@ -56,11 +57,14 @@ def train_ppo():
         net_env = env.Environment(all_cooked_time=all_cooked_time,
                               all_cooked_bw=all_cooked_bw)
 
-        model_ac = ActorCritic(A_DIM).type(dtype)
+        model_actor = Actor(A_DIM).type(dtype)
+        model_critic = Critic(A_DIM).type(dtype)
 
-        model_ac.train()
+        model_actor.train()
+        model_critic.train()
 
-        optimizer= optim.Adam(model_ac.parameters(), lr=LEARNING_RATE)
+        optimizer_actor = optim.RMSprop(model_actor.parameters(), lr=LEARNING_RATE_ACTOR)
+        optimizer_critic = optim.RMSprop(model_critic.parameters(), lr=LEARNING_RATE_CRITIC)
 
         # max_grad_norm = MAX_GRAD_NORM 
 
@@ -75,16 +79,14 @@ def train_ppo():
         epoch = 0
         time_stamp = 0
 
-        exploration_size = 16
-        episode_steps = 20
+        exploration_size = 4
+        episode_steps = 25
         update_num = 1
-        batch_size = exploration_size * episode_steps #64
-        gamma = 0.95
-        gae_param = 0.90
+        batch_size = 64
+        gamma = 0.99
+        gae_param = 0.95
         clip = 0.2
-        ent_coeff = 5.2
-        cl_coeff = 0.2
-        exploration_threhold = 0.05
+        ent_coeff = 0.98
         memory = ReplayMemory(exploration_size * episode_steps)
         # memory = ReplayMemory()
 
@@ -101,15 +103,9 @@ def train_ppo():
 
                 for step in range(episode_steps):
 
-                    prob, v = model_ac(state.unsqueeze(0).type(dtype))
+                    prob = model_actor(state.unsqueeze(0).type(dtype))
                     action = prob.multinomial(num_samples=1).detach()
-                    # seed_ = np.random.uniform(0,1)
-                    # if np.random.uniform(0,1) <= exploration_threhold:
-                    #     action = random.randint(0, 5)
-                    #     action = torch.tensor([[action]]).type(dlongtype)
-                    # else:
-                    #     action = prob.multinomial(num_samples=1)
-                    v = v.detach().cpu()
+                    v = model_critic(state.unsqueeze(0).type(dtype)).detach().cpu()
                     values.append(v)
 
                     bit_rate = int(action.squeeze().cpu().numpy())
@@ -140,7 +136,7 @@ def train_ppo():
                             - REBUF_PENALTY * rebuf \
                             - SMOOTH_PENALTY * np.abs(log_bit_rate - log_last_bit_rate)
                     reward_max = 2.67
-                    reward = float(max(min(reward, reward_max), -10*reward_max) / reward_max)
+                    reward = float(max(min(reward, reward_max), -4*reward_max) / reward_max)
                     rewards.append(reward)
                     rewards_comparison.append(torch.tensor([reward]))
 
@@ -179,8 +175,8 @@ def train_ppo():
                 # one last step
                 R = torch.zeros(1, 1)
                 if end_of_video == False:
-                    _, v = model_ac(state.unsqueeze(0).type(dtype))
-                    v = v.detach().cpu()
+                    v = model_critic(state.unsqueeze(0).type(dtype)).detach().cpu()
+                    # v = v.detach().cpu()
                     R = v.data
                 #================================结束一个ep========================================
                 # compute returns and GAE(lambda) advantages:
@@ -191,38 +187,42 @@ def train_ppo():
                     td = rewards[i] + gamma * values[i + 1].data[0, 0] - values[i].data[0, 0]
                     A = float(td) + gamma * gae_param * A
                     advantages.insert(0, A)
-                    R = A + values[i]
+                    # R = A + values[i]
+                    R = gamma * R + rewards[i]
                     returns.insert(0, R)
                 # store usefull info:
                 # memory.push([states[1:], actions[1:], rewards_comparison[1:], returns[1:], advantages[1:]])
-                memory.push([states[1:], actions[1:], returns[1:], advantages[1:]])
+                memory.push([states, actions, returns, advantages])
         
             # policy grad updates:
-            model_ac_old = ActorCritic(A_DIM).type(dtype)
-            model_ac_old.load_state_dict(model_ac.state_dict())
+            model_actor_old = Actor(A_DIM).type(dtype)
+            model_actor_old.load_state_dict(model_actor.state_dict())
+            model_critic_old = Critic(A_DIM).type(dtype)
+            model_critic_old.load_state_dict(model_critic.state_dict())
 
             ## actor update
             for update_step in range(update_num):
-                model_ac.zero_grad()
-                # model_critic.zero_grad()
+                model_actor.zero_grad()
+                model_critic.zero_grad()
 
                 # new mini_batch
                 # priority_batch_size = int(memory.get_capacity()/10)
-                # batch_states, batch_actions, _, batch_returns, batch_advantages = memory.sample(batch_size)
-                batch_states, batch_actions, batch_returns, batch_advantages = memory.pop(batch_size)
+                batch_states, batch_actions, batch_returns, batch_advantages = memory.sample(batch_size)
+                # batch_size = memory.return_size()
+                # batch_states, batch_actions, batch_returns, batch_advantages = memory.pop(batch_size)
 
                 # old_prob
-                probs_old, v_pre_old = model_ac_old(batch_states.type(dtype).detach())
-                # v_pre_old = model_critic_old(batch_states.type(dtype).detach())
+                probs_old = model_actor_old(batch_states.type(dtype).detach())
+                v_pre_old = model_critic_old(batch_states.type(dtype).detach())
                 prob_value_old = torch.gather(probs_old, dim=1, index=batch_actions.unsqueeze(1).type(dlongtype))
 
                 # new prob
-                probs, v_pre = model_ac(batch_states.type(dtype))
-                # v_pre = model_critic(batch_states.type(dtype))
+                probs = model_actor(batch_states.type(dtype))
+                v_pre = model_critic(batch_states.type(dtype))
                 prob_value = torch.gather(probs, dim=1, index=batch_actions.unsqueeze(1).type(dlongtype))
 
                 # ratio
-                ratio = prob_value / (1e-5 + prob_value_old)
+                ratio = prob_value / (1e-6 + prob_value_old)
 
                 ## non-clip loss
                 # surrogate_loss = ratio * batch_advantages.type(dtype)
@@ -236,11 +236,11 @@ def train_ppo():
                 vfloss1 = (v_pre - batch_returns.type(dtype)) ** 2
                 v_pred_clipped = v_pre_old + (v_pre - v_pre_old).clamp(-clip, clip)
                 vfloss2 = (v_pred_clipped - batch_returns.type(dtype)) ** 2
-                loss_value = cl_coeff * 0.5 * torch.mean(torch.max(vfloss1, vfloss2))
+                loss_value = 0.5 * torch.mean(torch.max(vfloss1, vfloss2))
                 # entropy
-                loss_ent = ent_coeff * torch.mean(probs * torch.log(probs + 1e-5))
+                loss_ent = ent_coeff * torch.mean(probs * torch.log(probs + 1e-6))
                 # total
-                policy_total_loss = (loss_clip_actor + loss_ent + loss_value)
+                policy_total_loss = loss_clip_actor + loss_ent
                 # print("hell0")
 
                 # copy the new model to old model?
@@ -248,13 +248,13 @@ def train_ppo():
                 # model_critic_old.load_state_dict(model_critic.state_dict())
 
                 # update 
-                optimizer.zero_grad()
-                # optimizer_critic.zero_grad()
-                policy_total_loss.backward(retain_graph=True)
+                optimizer_actor.zero_grad()
+                optimizer_critic.zero_grad()
+                policy_total_loss.backward()
                 # loss_clip_actor.backward(retain_graph=True)
-                # loss_value.backward(retain_graph=True)
-                optimizer.step()
-                # optimizer_critic.step()
+                loss_value.backward()
+                optimizer_actor.step()
+                optimizer_critic.step()
                 # print("hell0")
 
             ## test and save the model
@@ -263,15 +263,15 @@ def train_ppo():
             logging.info('Epoch: ' + str(epoch) +
                          ' Avg_policy_loss: ' + str(loss_clip_actor.detach().cpu().numpy()) +
                          ' Avg_value_loss: ' + str(loss_value.detach().cpu().numpy()) +
-                         ' Avg_entropy_loss: ' + str(loss_ent.detach().cpu().numpy()))
+                         ' Avg_entropy_loss: ' + str(A_DIM * loss_ent.detach().cpu().numpy()))
 
             if epoch % UPDATE_INTERVAL == 0:
                 logging.info("Model saved in file")
-                valid(model_ac, epoch, test_log_file, SUMMARY_DIR, 'ppo')
+                valid(model_actor, epoch, test_log_file)
                 # entropy_weight = 0.95 * entropy_weight
                 ent_coeff = 0.95 * ent_coeff
-# def main():
-#     train()
+def main():
+    train_ppo()
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
